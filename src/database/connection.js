5d6fe53242cde165f52b192b905ce7dd504ad6ee
mfontosco <59096300+mfontosco@users.noreply.github.com>
2022-10-27 08:33:04 +0100

    config modified

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,9 @@
 import Sequelize from 'sequelize'
 
-const sequelizeConnection = new Sequelize('soardigital', 'root', '', {host: 'localhost', dialect: 'mysql', port:3306,operatorAliases: false,});
+const sequelizeConnection = new Sequelize('soardigital', 
+'root', '', {host: 'localhost',
+ dialect: 'mysql',
+  operatorAliases: false,});
 
 export default sequelizeConnection;
 
