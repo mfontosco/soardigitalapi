a3dbc73a203aa2eee41d4a7f301f58b54a4301db
mfontosco <59096300+mfontosco@users.noreply.github.com>
2022-10-27 07:02:24 +0100

    port

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,6 @@
 import Sequelize from 'sequelize'
 
-const sequelizeConnection = new Sequelize('soardigital', 'root', '', {host: 'localhost', dialect: 'mysql', operatorAliases: false});
+const sequelizeConnection = new Sequelize('soardigital', 'root', '', {host: 'localhost', dialect: 'mysql', port:3306,operatorAliases: false,});
 
 export default sequelizeConnection;
 
